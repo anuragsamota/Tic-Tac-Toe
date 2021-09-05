b1637d828aa0e2b430a62d03ac4cf1da23d1ffa6
anuragsamota <anuragsamotasamota@gmail.com>
2021-09-05 18:40:27 +0530

    game fully working

--- script.js
+++ script.js
@@ -3,24 +3,22 @@ const reset_btn = document.getElementById("reset-button")
 const player_status = document.getElementById("player-status")
 const game_status = document.getElementById("game-status")
 
+const win_cases = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
 let occupied_moves = []
 let player1_moves = []
 let player2_moves = []
-let player1_moves_str = ""
-let player2_moves_str = ""
 let move_counter = 0
 let current_player = "Player 1"
-let match_staus = "On Going"
-let player1_win = false
-let player2_win = false
-const win_conditions = ["159","123","456","789","357","147","258","369"]
+let match_staus = "Match On Going"
 
 
-window.onclick = e=>{
-    if(match_staus === "On Going"){
+document.onclick = e=>{
+    if(match_staus === "Match On Going"){
         for(i=0;i<blocks.length;i++){
             if(e.target.id === blocks[i].id){
                 game(e.target)
+                break
             }
         }
     }
@@ -30,78 +28,83 @@ function game(element){
     let valid_move = true
 
     for(i=0;i<occupied_moves.length;i++){
-        if(element.id === occupied_moves[i]){
+        if(element.id == occupied_moves[i]){
             valid_move = false
+            break
         }
     }
     if(valid_move){
         if(current_player === "Player 1"){
             element.innerHTML = "O"
-            player1_moves.push(parseInt(element.id,10))
+            player1_moves.push(parseInt(element.id))
             current_player = "Player 2"
         }
         else{
             element.innerHTML = "X"
-            current_player = "Player 1"
             player2_moves.push(parseInt(element.id,10))
+            current_player = "Player 1"
         }
         player_status.innerHTML = current_player + " Move"
-        
+
         move_counter++
         if(move_counter>=9){
             player_status.innerHTML = "Moves Completed"
-            
+
         }
+        occupied_moves.push(parseInt(element.id))
+        winCheck()
     }
     else{
         //code for invalid move
         alert("invalid move")
     }
-    occupied_moves.push(element.id)
-    winCheck(element)
-    drawCheck()
 }
-function winCheck(element){
-    //sorting player 1 and 2 array
+
+
+function winCheck(){
     player1_moves.sort(function(a,b){return a-b})
     player2_moves.sort(function(a,b){return a-b})
-    player1_moves_str = ""
-    player2_moves_str = ""
-    for(i=0;i<player1_moves.length;i++){
-        player1_moves_str += player1_moves[i]
-    }
-    for(i=0;i<player2_moves.length;i++){
-        player2_moves_str += player2_moves[i]
-    }
-    //player 1
-    for(i=0;i<win_conditions.length;i++){
-        if(player1_moves_str == win_conditions[i]){
-            player1_win = true
+
+    for(i = 0; i < 8; i++){
+        let player1_correct = 0
+        let player2_correct = 0
+        for(j=0;j<3;j++){
+            //player 1
+            for(k=0;k<player1_moves.length;k++){
+                if(player1_moves[k]==win_cases[i][j]){
+                    player1_correct++
+                }
+            }
+            //player 2
+            for(k=0;k<player2_moves.length;k++){
+                if(player2_moves[k]==win_cases[i][j]){
+                    player2_correct++
+                }
+            }
+        }
+        //checking
+        if(player1_correct>=3){
+            match_staus = "Player 1 Wins"
+            if(move_counter<9){
+                player_status.innerHTML = "Game Over"
+            }
+        }
+        if(player2_correct>=3){
+            match_staus = "Player 2 Wins"
+            if(move_counter<9){
+                player_status.innerHTML = "Game Over"
+            }
         }
     }
-    //player 2
-    for(i=0;i<win_conditions.length;i++){
-        if(player2_moves_str == win_conditions[i]){
-            player2_win = true
+    //for draw
+    if(move_counter>=9){
+        if(match_staus == "Match On Going"){
+            match_staus = "Match Draw"
         }
-    }
-    if(player1_win){
-        match_staus = "Player 1 Wins"
-    }
-    if(player2_win){
-        match_staus = "Player 2 Wins"
     }
     game_status.innerHTML = match_staus
 }
 
 reset_btn.onclick = function(){
     location.reload()
-}
-function drawCheck(){
-    if(move_counter>=9){
-        if(!(player1_win||player2_win)){
-            match_staus = "Draw"
-        }
-    }
-    game_status.innerHTML = match_staus
 }
